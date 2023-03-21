2b424d122aab79575f4bc992ea7cdd643729e8fc
NycolasBC <nycolascarvalho14@gmail.com>
2023-03-21 17:18:30 -0300

    first commit

--- Exercicio04.cs
+++ Exercicio04.cs
@@ -10,7 +10,33 @@ namespace Atv03_AlgoritmoAvancado
     {
         public static void ExercicioAlgoritmo04()
         {
+            Console.WriteLine("\nWxercício 03:\n");
 
+            int[,] matrizA = new int[3, 3];
+            int[,] matrizB = new int[3, 3];
+
+            for (int i = 0; i < matrizA.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrizA.GetLength(0); j++)
+                {
+                    Console.WriteLine($"\nDigite o valor na posição [{i},{j}] da Matriz A.");
+                    matrizA[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine($"Digite o valor na posição [{i},{j}] da Matriz B.");
+                    matrizB[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            int[,] somaMatriz = new int[3, 3];
+
+            for (int i = 0; i < matrizB.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrizB.GetLength(0); j++)
+                {
+                    somaMatriz[i, j] = matrizA[i, j] + matrizB[i, j];
+                    Console.WriteLine($"A soma das linhas e colunas de ambas a matrizes na posição [{i},{j}] é {somaMatriz[i, j]}");
+                }
+            }
         }
     }
 }
